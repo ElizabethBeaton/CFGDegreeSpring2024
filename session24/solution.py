b5b7f37344b98ee1275155fa1153f44894006f32
*Elizabeth <*elizabethbeaton23@outlook.com*>
2024-04-07 17:59:22 +0100

    unit testign task and solution

---
+++ session24/solution.py
@@ -0,0 +1,17 @@
+def solution_function(given_int: int) -> str:
+    if (2 <= given_int <= 6 or  20 < given_int) and not given_int % 2:
+        return 'Blue'
+    else:
+        return 'Red'
+def solution_to_different_problem():
+    pass
+
+# def solution_function(given_int: int) -> str:
+#     if given_int % 2:
+#         return 'Red'
+#     elif 2 <= given_int <= 6 or  20 < given_int:
+#         return 'Blue'
+#     else:
+#         return 'Red'
+# def solution_to_different_problem():
+#     pass
---
+++ session24/unit_testing_examples.py
@@ -0,0 +1,48 @@
+"""
+TASK
+given an integer x, write a program that does the following:
+
+if x is odd, return "red"
+if x is even, and in the range of 2 to 5 inclusive, return 'blue
+if x is even, and in the range 6 to 20 inclusive, return "red"
+if x is even, and greater than 20, return "blue"
+
+constraint input will always be an integer, between 1 and 100
+
+21 red
+5 red
+20 red
+2 blue
+99 red
+77 red
+1 red
+24 blue
+6
+
+"""
+
+from unittest import mock, TestCase, main
+from .solution import solution_function
+
+class TestRedOrBlueFunction(TestCase):
+    def test_odd_numbers(self):
+        # 5
+        expected_result = 'Red'
+        result = solution_function(5)
+        self.assertEqual(result, expected_result)
+
+    def test_even_between_2_5(self):
+        expected_result = 'Blue'
+        actual_result = solution_function(4)
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_even_between_6_20(self):
+        expected_result = 'Red'
+        actual_result = solution_function(10)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_even_greater_than_20(self):
+        expected_result = 'Blue'
+        actual_result = solution_function(44)
+        self.assertEqual(expected_result, actual_result)
